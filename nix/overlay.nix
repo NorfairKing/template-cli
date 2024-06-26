bd057a9cf30552f3d95be2d23268766804376cda
Tom Sydney Kerckhove <syd@cs-syd.eu>
2024-06-26 18:19:37 +0200

    Upgrade to nixos-24.05 and lts-22.22

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -29,48 +29,63 @@ with final.haskell.lib;
       final.haskellPackages.fooBarPackages;
 
   haskellPackages = prev.haskellPackages.override (old: {
-    overrides = final.lib.composeExtensions (old.overrides or (_: _: { }))
-      (
-        self: _:
-          let
-            fooBarPkg = name:
-              buildFromSdist (overrideCabal (self.callPackage (../${name}/default.nix) { }) (old: {
-                configureFlags = (old.configureFlags or [ ]) ++ [
-                  # Optimisations
-                  "--ghc-options=-O2"
-                  # Extra warnings
-                  "--ghc-options=-Wall"
-                  "--ghc-options=-Wincomplete-uni-patterns"
-                  "--ghc-options=-Wincomplete-record-updates"
-                  "--ghc-options=-Wpartial-fields"
-                  "--ghc-options=-Widentities"
-                  "--ghc-options=-Wredundant-constraints"
-                  "--ghc-options=-Wcpp-undef"
-                  "--ghc-options=-Werror"
-                ];
-                doBenchmark = true;
-                doHaddock = false;
-                doCoverage = false;
-                doHoogle = false;
-                doCheck = false; # Only for coverage
-                hyperlinkSource = false;
-                enableLibraryProfiling = false;
-                enableExecutableProfiling = false;
-                # Ugly hack because we can't just add flags to the 'test' invocation.
-                # Show test output as we go, instead of all at once afterwards.
-                testTarget = (old.testTarget or "") + " --show-details=direct";
-              }));
-            fooBarPkgWithComp =
-              exeName: name:
-              self.generateOptparseApplicativeCompletions [ exeName ] (fooBarPkg name);
+    overrides = final.lib.composeExtensions (old.overrides or (_: _: { })) (self: super:
+      let
+        fooBarPkg = name:
+          buildFromSdist (overrideCabal (self.callPackage (../${name}/default.nix) { }) (old: {
+            configureFlags = (old.configureFlags or [ ]) ++ [
+              # Optimisations
+              "--ghc-options=-O2"
+              # Extra warnings
+              "--ghc-options=-Wall"
+              "--ghc-options=-Wincomplete-uni-patterns"
+              "--ghc-options=-Wincomplete-record-updates"
+              "--ghc-options=-Wpartial-fields"
+              "--ghc-options=-Widentities"
+              "--ghc-options=-Wredundant-constraints"
+              "--ghc-options=-Wcpp-undef"
+              "--ghc-options=-Werror"
+            ];
+            doBenchmark = true;
+            doHaddock = false;
+            doCoverage = false;
+            doHoogle = false;
+            doCheck = false; # Only for coverage
+            hyperlinkSource = false;
+            enableLibraryProfiling = false;
+            enableExecutableProfiling = false;
+            # Ugly hack because we can't just add flags to the 'test' invocation.
+            # Show test output as we go, instead of all at once afterwards.
+            testTarget = (old.testTarget or "") + " --show-details=direct";
+          }));
+        fooBarPkgWithComp =
+          exeName: name:
+          self.generateOptparseApplicativeCompletions [ exeName ] (fooBarPkg name);
 
-            fooBarPackages = {
-              foo-bar-cli = fooBarPkgWithComp "foo-bar" "foo-bar-cli";
-            };
-          in
-          {
-            inherit fooBarPackages;
-          } // fooBarPackages
-      );
+        fooBarPackages = {
+          foo-bar-cli = fooBarPkgWithComp "foo-bar" "foo-bar-cli";
+        };
+        fixGHC = pkg:
+          if final.stdenv.hostPlatform.isMusl
+          then
+            pkg.override
+              {
+                # To make sure that executables that need template
+                # haskell can be linked statically.
+                enableRelocatedStaticLibs = true;
+                enableShared = false;
+                enableDwarf = false;
+              }
+          else pkg;
+
+      in
+      {
+        ghc = fixGHC super.ghc;
+        buildHaskellPackages = old.buildHaskellPackages.override (oldBuildHaskellPackages: {
+          ghc = fixGHC oldBuildHaskellPackages.ghc;
+        });
+        inherit fooBarPackages;
+      } // fooBarPackages
+    );
   });
 }
